5f7103a835779b3be0d070c887ad2c4d74d13ad1
umutinceer <mumutinceer@gmail.com>
2021-07-06 19:13:57 +0300

    clear button, color picker, draw functions implemented

--- app.js
+++ app.js
@@ -1,27 +1,27 @@
-const toggleErase = document.querySelector("#toggleErase");
-const toggleRainbow = document.querySelector("#toggleRainbow");
+const btnToggleErase = document.querySelector("#toggleErase");
+const btnToggleRainbow = document.querySelector("#toggleRainbow");
 const colorPicker = document.querySelector("#colorPicker");
 const slider = document.querySelector(".slider");
 const paraGridSize = document.querySelector("#paraGridSize");
-const clearAll = document.querySelector("#clearAll");
+const btnClearAll = document.querySelector("#clearAll");
 const container = document.querySelector("#container");
 
 let gridSize=1;
 
 setGridSize(gridSize);
 
-slider.addEventListener("input",(gridSize) => {
+slider.addEventListener("input",() => {
     gridSize = slider.value;
-    setGridSize(gridSize);
+    setGridSize();
 });
 
-function setGridSize(gridSize)
+function setGridSize()
 {
     paraGridSize.textContent = `Grid Size: ${gridSize}x${gridSize} `;
-    createBoard(gridSize,container);
+    createBoard(container);
 }
 
-function createBoard(gridSize,container)
+function createBoard(container)
 {
     container.innerHTML = "";
 
@@ -29,8 +29,60 @@ function createBoard(gridSize,container)
     {
         let gridDiv = document.createElement("div");
         gridDiv.className = "gridClass";
+        gridDiv.addEventListener("mouseover", () => {
+            draw(colorPicked,gridDiv);
+        });
         container.append(gridDiv);
     }
 
     container.style.cssText = `grid-template-columns: repeat(${gridSize},auto); grid-template-rows: repeat(${gridSize},auto);`;
 }
+
+
+
+let colorPicked = colorPicker.value;
+
+colorPicker.addEventListener("change", () => {
+    colorPicked = colorPicker.value;
+});
+
+function draw(colorPicked, gridElement)
+{
+    gridElement.style.backgroundColor = colorPicked;
+}
+
+
+btnClearAll.addEventListener("click", () => {
+    clearAll(container);
+});
+
+function clearAll(container)
+{
+    let list = container.childNodes;
+    console.log(gridSize);
+
+    for(let i=0; i<gridSize**2; i++)
+    {
+        list[i].style.backgroundColor = "white";
+    }
+}
+
+/*
+
+    TOGGLE ERASE 1 İKEN SİLSİN   (YADA 2 YE BÖLÜNEBİLİYORKEN DE OLUR FARKETMEZ)
+    0 İKEN COLOR PİCKERADN GELEN DEĞERLE ÇİZSİN
+
+
+let toggleEraseCounter = 0;
+
+toggleErase.addEventListener("click", () => {
+
+    
+
+});
+
+
+function togErase()
+{
+
+}*/
